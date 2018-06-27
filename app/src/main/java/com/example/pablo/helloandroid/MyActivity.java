356cbda0f4b5878ea3995740e00f7c473d58d616
Pablo Bahillo <bahillo.ortego.pablo@gmail.com>
2018-06-27 19:58:11 +0200

    Añadida la funcionalidad para los botones Button1 y Button2, además de la correcta colocación de los mismos en la pantalla
    Uso de Log.i() y Toast.wirteText()

--- app/src/main/java/com/example/pablo/helloandroid/MyActivity.java
+++ app/src/main/java/com/example/pablo/helloandroid/MyActivity.java
@@ -2,6 +2,9 @@ package com.example.pablo.helloandroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+import android.util.Log;
 
 public class MyActivity extends AppCompatActivity {
 
@@ -9,5 +12,19 @@ public class MyActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.my_layout);
+
+        //Prueba de Toast y Log
+        Toast.makeText(this, "Puedes verme?", Toast.LENGTH_SHORT).show();
+        Log.i("info", "App creada: ");
+    }
+
+    public void topClick(View v){
+        Toast.makeText(this, "Top button clicked", Toast.LENGTH_SHORT).show();
+        Log.i("info", "topClick: ");
+    }
+
+    public void bottomClick(View v){
+        Toast.makeText(this,"Bottom button clicked", Toast.LENGTH_SHORT).show();
+        Log.i("info", "bottomClick: ");
     }
 }
